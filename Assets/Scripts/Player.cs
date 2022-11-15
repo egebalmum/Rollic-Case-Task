fa3885a712c0d21cc0d15c68980dc25044e7bb3d
Ege Balmum <ebalmum19@ku.edu.tr>
2022-11-15 04:28:10 +0300

    Designed physic system

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,19 +1,28 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] public float speed = 5;
+    //Public Declared Values
+    [SerializeField] public float horizontalSpeed = 5;
+    [SerializeField] public float verticalSpeed = 7;
     
+    //Private Declared Values
+    private Vector3 _startingPosition;
+
+    //Cached Values
     private Rigidbody _rigidbody;
+    
     void Start()
     {
+        _startingPosition = transform.position;
         _rigidbody = GetComponent<Rigidbody>();
     }
 
     
-    void Update()
+    void FixedUpdate()
     {
         Move();
     }
@@ -21,8 +30,12 @@ public class Player : MonoBehaviour
     void Move()
     {
         var horizontalVector = Input.GetAxisRaw("Horizontal");
-        var verticalVector = Input.GetAxisRaw("Vertical");
-        var totalVector = new Vector3(horizontalVector, 0, verticalVector);
-        _rigidbody.position += totalVector * (speed * Time.deltaTime);
+        var totalVector = new Vector3(horizontalVector*horizontalSpeed, 0, verticalSpeed);
+        _rigidbody.MovePosition(_rigidbody.position + (totalVector * (Time.fixedDeltaTime)));
+        if (Mathf.Abs(_rigidbody.position.z - _startingPosition.z) >= 30 && verticalSpeed!= 0)
+        {
+            _rigidbody.position = new Vector3(_rigidbody.position.x, _rigidbody.position.y, _startingPosition.z+30);
+            verticalSpeed = 0;
+        }
     }
 }
